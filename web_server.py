8dd8a2962ebad9d282d1f723c34b3752dacd33a1
denisova-ok <denisova.olga.k@gmail.com>
2024-11-12 04:28:02 +0300

    remove trash

--- web_server.py
+++ web_server.py
@@ -1,10 +1,3 @@
-# from http.server import HTTPServer, CGIHTTPRequestHandler
-#
-# server_address = ("", 8000)
-#
-# httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-#
-# httpd.serve_forever()
 import flask
 from flask import Flask
 import json
