9c0e4caab378378e1ba23f529b3ae9af347d62ea
Васильева Ольга <90911814+Silence564@users.noreply.github.com>
2024-11-27 18:01:47 +0300

    module window 2/3

--- static/js/player_page.js
+++ static/js/player_page.js
@@ -17,11 +17,35 @@ window.addEventListener("load", async function(){
 let wishes = document.getElementById("wishess");
 let wDialog = document.getElementById("WDialog");
 const Button = document.getElementById("close");
+const Button1 = document.getElementById("close1");
+
+let loadB = document.getElementById("loaad");
+let wDialog2 = document.getElementById("WDialog2");
+
+let info = document.getElementById("text");
+let wDialog3 = document.getElementById("WDialog3");
+const Button2 = document.getElementById("close2");
 
 Button.addEventListener('click', () => {
     wDialog.close();
 })
 
+Button1.addEventListener('click', () => {
+    wDialog2.close();
+})
+
+Button2.addEventListener('click', () => {
+    wDialog3.close();
+})
+
 wishes.addEventListener('click', ()=>{
     wDialog.showModal();
+})
+
+loadB.addEventListener('click', ()=>{
+    wDialog2.showModal();
+})
+
+info.addEventListener('click', ()=>{
+    wDialog3.showModal();
 })
