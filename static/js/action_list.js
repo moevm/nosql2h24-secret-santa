adf3ff8558902ec9d482c6077efcd0288be86067
Васильева Ольга <90911814+Silence564@users.noreply.github.com>
2024-12-03 12:25:47 +0300

    page site_statistics and page action

---
+++ static/js/action_list.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", async function(){
+    await fetch(new URL('/get_list_people', 'http://localhost:8000').href, {
+                method: "GET",
+                headers: {
+                'Accept': 'application/json',
+                "Content-type": "application/json; charset=UTF-8"
+    }})
+    .then(res => res.json())
+    .then(content => {
+        let list = this.document.getElementById('list_action');
+        for (let i =0; i < content.length; i++){
+            let div = document.createElement('div');
+            div.style.display = "flex";
+            div.style.flexDirection = "row";
+            div.style.justifyContent = "space-between";
+            let text1 = this.document.createElement('div');
+            text1.textContent = i;
+
+            text1.style.color = "black";
+            text1.style.fontSize = "40px";
+            text1.style.fontWeight = "400";
+            text1.style.textAlign = "center";
+            text1.style.wordWrap = "break-word";
+            text1.style.padding = "1% 1%";
+
+            div.appendChild(text1);
+
+            div.style.height = "98px";
+            div.style.background = "rgba(105.97, 207.54, 225.46, 0.22)";
+            div.style.borderTopLeftRadius = '4px';
+            div.style.borderTopRightRadius = '4px';
+            div.style.borderBottomLeftRadius = '4px';
+            div.style.borderBottomRightRadius = '4px';
+            div.style.border = "3px black solid";
+            div.style.marginTop = "1%";
+            div.id = i + "action";
+            list.appendChild(div);
+        }
+    })
+});
